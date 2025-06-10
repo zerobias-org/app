e2af6c40edad6c91452fde66653ade3e54cb2dd0
Clark Stacer <clark@w3geekery.com>
2025-06-10 10:10:09 -0700

    chore: update for testing deploy

--- package/zerobias/example-nextjs/app/page.tsx
+++ package/zerobias/example-nextjs/app/page.tsx
@@ -3,14 +3,18 @@
 import MainTabs from "@/components/ui/mainTabs";
 import CreateApiKeyForm from "@/components/forms/createApiKeyForm";
 import AppToolbar from "@/components/ui/appToolbar";
+import { useCurrentUser } from "@/context/CurrentUserContext";
 
 export default function Home() {
-  
+  const { user, org, loading } = useCurrentUser();
+
+  if (!loading && !user) {
+    console.log('PEBCAK');
+  }
     return (
       <>
           <AppToolbar/>
-
-
+          
           <CreateApiKeyForm />
 
           <div className="content-wrap">
