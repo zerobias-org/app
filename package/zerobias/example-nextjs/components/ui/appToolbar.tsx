81793f17bba18378b3efd29d4e7c2c96732fdec4
Clark Stacer <clark@w3geekery.com>
2025-06-18 15:41:32 -0700

    chore: tweaks, updates for image paths

--- package/zerobias/example-nextjs/components/ui/appToolbar.tsx
+++ package/zerobias/example-nextjs/components/ui/appToolbar.tsx
@@ -7,7 +7,7 @@ export default function AppToolbar() {
     <div className="toolbar">
 
       <div className="app-bar">
-        <Link href={'/'}><Image src="/zerobias_white.svg" alt="ZeroBias" height="28" width="145" /></Link>
+        <Link href={process.env.NEXT_PUBLIC_IS_LOCAL_DEV === 'true' ? '/' : '/example-nextjs'}><Image src={process.env.NEXT_PUBLIC_IS_LOCAL_DEV === 'true' ? '/zerobias_white.svg' : '/example-nextjs/zerobias_white.svg'} alt="ZeroBias" height="28" width="145" /></Link>
         <h1>ZeroBias Client API Demo</h1>
         <span className="flexFill"></span>
 
