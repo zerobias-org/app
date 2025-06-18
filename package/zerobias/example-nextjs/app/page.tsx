aaa58856923dcee669509fdf479b5e56b93376bd
Clark Stacer <clark@w3geekery.com>
2025-06-18 15:41:32 -0700

    chore: tweaks, updates for image paths

--- package/zerobias/example-nextjs/app/page.tsx
+++ package/zerobias/example-nextjs/app/page.tsx
@@ -7,7 +7,14 @@ import { useCurrentUser } from "@/context/CurrentUserContext";
 import { Suspense } from "react";
 import { X, ShoppingCart, Blocks, ListCollapse } from 'lucide-react';
 import Link from "next/link";
+import { Montserrat } from "next/font/google";
 
+
+  const montserrat = Montserrat({
+    variable: "--font-montserrat",
+    subsets: ["latin"],
+  })
+  
 export default function Home() {
   const { user, org, loading, action, setAction } = useCurrentUser();
 
@@ -20,15 +27,15 @@ export default function Home() {
     <>
       <AppToolbar/>
       
-      <div className="content-wrap">
+
         <div className="content-wrapper flexColumn gap16 demo-items-wrapper">
           <h2>Demos</h2>
 
           <div className="demo-item">
             <div className="item-header">
-              <Link href={'/products-demo'}><ShoppingCart size={40} /><span className="item-title">Products List Demo</span></Link>
+              <Link href={'/products-demo'}><ShoppingCart size={24} /><span className="item-title">Products List Demo</span></Link>
             </div>
-            <div className="item-subtitle">
+            <div className={`${montserrat.className} item-subtitle`}>
               <p>This example shows the use of our Catalog API <code>zerobiasClientApi.portalClient.getProductApi().search()</code> endpoint to get a list of products from the ZeroBias Catalog.</p>
             </div>
             <div className="item-actions">
@@ -38,9 +45,9 @@ export default function Home() {
 
           <div className="demo-item">
             <div className="item-header">
-              <Link href={'/module-demo'}><Blocks size={40} /><span className="item-title">Module Usage Demo</span></Link>
+              <Link href={'/module-demo'}><Blocks size={24} /><span className="item-title">Module Usage Demo</span></Link>
             </div>
-            <div className="item-subtitle">
+            <div className={`${montserrat.className} item-subtitle`}>
               <p>This example shows the use of our GitHub Module to make calls to retrieve a list of <i>your</i> organization's GitHub Repositories.</p>
             </div>
             <div className="item-actions">
@@ -50,9 +57,9 @@ export default function Home() {
 
           <div className="demo-item">
             <div className="item-header">
-              <Link href={'/pkv-demo'}><ListCollapse size={40} /><span className="item-title">PKV Demo</span></Link>
+              <Link href={'/pkv-demo'}><ListCollapse size={24} /><span className="item-title">PKV Demo</span></Link>
             </div>
-            <div className="item-subtitle">
+            <div className={`${montserrat.className} item-subtitle`}>
               <p>This example shows the use of our Principal Key-Value API <code>zerobiasClientApi.danaClient.getPkvApi()</code> endpoints to manipulate the storage of key-value pairs</p>
             </div>
             <div className="item-actions">
@@ -61,7 +68,7 @@ export default function Home() {
           </div>
 
         </div>
-      </div>
+
 
       <footer>
       
